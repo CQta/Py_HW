774524f9620ab0e2b44009439c58c171f4cdae69
CQta <86158589+CQta@users.noreply.github.com>
2022-12-16 20:51:03 +0300

    Delete Prog 5.py

--- Prog 5.py
+++
@@ -1,6 +0,0 @@
-import math
-x = int(input("Введите x для 1 числа "))
-y = int(input("Введите y для 1 числа "))
-x2 = int(input("Введите x для 2 числа "))
-y2 = int(input("Введите y для 2 числа "))
-print(round(math.sqrt(((x2-x)**2)+(y2-y)**2), 3))
